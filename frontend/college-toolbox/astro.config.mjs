dca0d2420d7027f63f466001f49d1dd98ad5ed89
Poggecci <alejandro.cruzado@upr.edu>
2023-09-19 14:44:47 -0400

    fix base path

--- frontend/college-toolbox/astro.config.mjs
+++ frontend/college-toolbox/astro.config.mjs
@@ -10,7 +10,7 @@ const DEV_PORT = 2121;
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://uprm-inso4116-2023-2024-s1.github.io' : `http://localhost:${DEV_PORT}`,
-  base: process.env.CI ? '/semester-project-college-toolbox/frontend/college-toolbox' : undefined,
+  base: process.env.CI ? '/semester-project-college-toolbox' : undefined,
   server: {
     /* Dev. server only */
     port: DEV_PORT
