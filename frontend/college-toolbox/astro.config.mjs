0cea99497e9766ae4d799b2c0205c594ec8b58b8
Poggecci <alejandro.cruzado@upr.edu>
2023-09-19 13:58:52 -0400

    Reinstall dependencies and update pages URL

--- frontend/college-toolbox/astro.config.mjs
+++ frontend/college-toolbox/astro.config.mjs
@@ -3,29 +3,17 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+
 const DEV_PORT = 2121;
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.CI
-		? 'https://uprm-inso4116-2023-2024-s1.github.io.github.io'
-		: `http://localhost:${DEV_PORT}`,
-	base: process.env.CI ? '/semester-project-college-toolbox/frontend/college-toolbox' : undefined,
-
-	// output: 'server',
-
-	/* Like Vercel, Netlify,… Mimicking for dev. server */
-	// trailingSlash: 'always',
-
-	server: {
-		/* Dev. server only */
-		port: DEV_PORT,
-	},
-
-	integrations: [
-		//
-		sitemap(),
-		tailwind(),
-		[react()]
-	],
+  site: process.env.CI ? 'https://uprm-inso4116-2023-2024-s1.github.io' : `http://localhost:${DEV_PORT}`,
+  base: process.env.CI ? '/semester-project-college-toolbox/frontend/college-toolbox' : undefined,
+  server: {
+    /* Dev. server only */
+    port: DEV_PORT
+  },
+  integrations: [process.platform !== 'win32' && sitemap(), tailwind(), react()]
 });
