9a3b972c43d570282700e63f03ee371219609aed
BreannaGSegarra <breanna.segarra@upr.edu>
2023-09-20 16:58:20 -0400

    Move UserProfile into separate directory

--- backend/src/models/responses/register.py
+++ backend/src/models/responses/register.py
@@ -1,6 +1,6 @@
 # src/models/responses/register.py
 from pydantic import BaseModel
-from src.models.responses.login import UserProfile
+from src.models.common.user_profile import UserProfile
 
 
 class RegisterResponse(BaseModel):
