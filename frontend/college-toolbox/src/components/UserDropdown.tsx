701bf8a8e58e9861db7baf99d99fb7c86ad4da2d
poggecci <alejandro.cruzado@upr.edu>
2023-09-23 00:53:08 -0400

    Fix settings button not routing to settings page

--- frontend/college-toolbox/src/components/UserDropdown.tsx
+++ frontend/college-toolbox/src/components/UserDropdown.tsx
@@ -86,7 +86,7 @@ const UserDropdown = () => {
         <ul className="py-1" role="none">
             <li>
                 <a
-                href="#"
+                href={url('settings')}
                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                 role="menuitem"
                 >
