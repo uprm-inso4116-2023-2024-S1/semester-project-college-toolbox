ff30009a6f002b8e6d224a9f692898b114ca0b5e
Poggecci <alejandro.cruzado@upr.edu>
2023-09-29 19:31:06 -0400

    Format Files

--- ssh-scraper/utils.py
+++ ssh-scraper/utils.py
@@ -19,12 +19,15 @@ class TimeBlock:
     def __repr__(self):
         return f"{self.course_id}-{self.section} {self.start_time.strftime('%I:%M %p')} - {self.end_time.strftime('%I:%M %p')} {self.room}"
 
+
 def make_empty_list():
     return []
+
+
 @dataclass
 class WeekSchedule:
-    monday: list =  field(default_factory=make_empty_list)
-    tuesday: list =  field(default_factory=make_empty_list)
+    monday: list = field(default_factory=make_empty_list)
+    tuesday: list = field(default_factory=make_empty_list)
     wednesday: list = field(default_factory=make_empty_list)
     thursday: list = field(default_factory=make_empty_list)
     friday: list = field(default_factory=make_empty_list)
@@ -35,49 +38,100 @@ class WeekSchedule:
 
 def get_course_sections(course_id: str, term: Term, year: int) -> list[CourseSection]:
     with Session(engine) as session:
-        return session.query(CourseSection).filter(and_(and_(CourseSection.course_id == course_id, CourseSection.term == term.value), CourseSection.year == year)).all()
+        return (
+            session.query(CourseSection)
+            .filter(
+                and_(
+                    and_(
+                        CourseSection.course_id == course_id,
+                        CourseSection.term == term.value,
+                    ),
+                    CourseSection.year == year,
+                )
+            )
+            .all()
+        )
+
 
 def get_room_schedules(course_section_id: int) -> list[RoomSchedule]:
     with Session(engine) as session:
-        return session.query(RoomSchedule).filter(RoomSchedule.course_section_id == course_section_id).all()
+        return (
+            session.query(RoomSchedule)
+            .filter(RoomSchedule.course_section_id == course_section_id)
+            .all()
+        )
 
-def get_course_section_time_blocks(course_id: str, section: str, term: Term, year: int) -> list[TimeBlock]:
+
+def get_course_section_time_blocks(
+    course_id: str, section: str, term: Term, year: int
+) -> list[TimeBlock]:
     time_blocks = []
-    
+
     with Session(engine) as session:
-        section = session.query(CourseSection).filter(and_(and_(CourseSection.course_id == course_id, CourseSection.section == section), and_(CourseSection.term == term.value, CourseSection.year == year))).first()
+        section = (
+            session.query(CourseSection)
+            .filter(
+                and_(
+                    and_(
+                        CourseSection.course_id == course_id,
+                        CourseSection.section == section,
+                    ),
+                    and_(CourseSection.term == term.value, CourseSection.year == year),
+                )
+            )
+            .first()
+        )
         schedules = get_room_schedules(section.id)
 
         for schedule in schedules:
             if schedule.days is None:
                 continue
             for day in schedule.days:
-                time_blocks.append(TimeBlock(course_id, section.section, schedule.room, day, schedule.start_time, schedule.end_time))
+                time_blocks.append(
+                    TimeBlock(
+                        course_id,
+                        section.section,
+                        schedule.room,
+                        day,
+                        schedule.start_time,
+                        schedule.end_time,
+                    )
+                )
 
     return time_blocks
 
+
 def validate_course_id(course_id: str):
     with Session(engine) as session:
-        return session.query(CourseSection).filter(CourseSection.course_id == course_id).first() is not None
+        return (
+            session.query(CourseSection)
+            .filter(CourseSection.course_id == course_id)
+            .first()
+            is not None
+        )
+
 
 # main call function for schedule generation
-def make_all_schedules(courses : list[str], term : Term, year: int) -> list[WeekSchedule]:
-    if len(courses) == 0: return []
+def make_all_schedules(courses: list[str], term: Term, year: int) -> list[WeekSchedule]:
+    if len(courses) == 0:
+        return []
 
     return make_all_schedules_helper(WeekSchedule(), courses, term, year, set(), 0)
-  
+
+
 # Recursively adds and removes courses and course sections using backtracking.
 # This ensures that all possible schedules will be checked for validity.
 # Valid meaning that there were no conflicts and that all courses were added.
 # If 5 course IDs were passed by parameter, all shceudles must strictly have 5 courses.
 # The function return a list of all possible schedules that were valid.
-def make_all_schedules_helper(curr_week_schedule : WeekSchedule, \
-                              courses : list[str],               \
-                              term : Term,                       \
-                              year: int,                         \
-                              courses_scheduled : set,           \
-                              index : int) -> list[WeekSchedule]:
-
+def make_all_schedules_helper(
+    curr_week_schedule: WeekSchedule,
+    courses: list[str],
+    term: Term,
+    year: int,
+    courses_scheduled: set,
+    index: int,
+) -> list[WeekSchedule]:
     if len(courses_scheduled) == len(courses):
         return [curr_week_schedule]
     if index >= len(courses):
@@ -85,57 +139,83 @@ def make_all_schedules_helper(curr_week_schedule : WeekSchedule, \
 
     all_schedules = []
     for course_section in get_course_sections(courses[index], term, year):
-        time_blocks = get_course_section_time_blocks(course_section.course_id, course_section.section, Term(course_section.term), course_section.year)
-        
-        updated_schedule, time_conflict_detected = add_course_to_schedule(curr_week_schedule, time_blocks)
+        time_blocks = get_course_section_time_blocks(
+            course_section.course_id,
+            course_section.section,
+            Term(course_section.term),
+            course_section.year,
+        )
+
+        updated_schedule, time_conflict_detected = add_course_to_schedule(
+            curr_week_schedule, time_blocks
+        )
 
         if not time_conflict_detected:
             courses_scheduled.add(course_section.course_id)
-            all_schedules += make_all_schedules_helper(updated_schedule, courses, term, year, courses_scheduled, index + 1) 
+            all_schedules += make_all_schedules_helper(
+                updated_schedule, courses, term, year, courses_scheduled, index + 1
+            )
             courses_scheduled.remove(course_section.course_id)
 
     return all_schedules
-    
+
+
 # Adds all the time blocks to their corresponfing day for a specific course section.
 # If just one time block conflicts with another section' time block (added in a previous iteration),
 # the function is terminated and returns the original WeekSchedule before any changes were made.
-def add_course_to_schedule(curr_week_schedule : WeekSchedule, course_section_time_blocks : list[TimeBlock]) -> tuple[WeekSchedule, bool]:
+def add_course_to_schedule(
+    curr_week_schedule: WeekSchedule, course_section_time_blocks: list[TimeBlock]
+) -> tuple[WeekSchedule, bool]:
     copy_week_schedule = copy.deepcopy(curr_week_schedule)
 
     time_conflict_flag = False
 
     for section_block in course_section_time_blocks:
         day = section_block.day
-        if day == 'L':
-            time_conflict_flag = add_time_block_to_day(copy_week_schedule.monday, section_block)
-            
-        elif day == 'M':
-            time_conflict_flag = add_time_block_to_day(copy_week_schedule.tuesday, section_block)
-            
-        elif day == 'W':
-            time_conflict_flag = add_time_block_to_day(copy_week_schedule.wednesday, section_block)
-            
-        elif day == 'J':
-            time_conflict_flag = add_time_block_to_day(copy_week_schedule.thursday, section_block)
-            
-        elif day == 'V':
-            time_conflict_flag = add_time_block_to_day(copy_week_schedule.friday, section_block)
+        if day == "L":
+            time_conflict_flag = add_time_block_to_day(
+                copy_week_schedule.monday, section_block
+            )
+
+        elif day == "M":
+            time_conflict_flag = add_time_block_to_day(
+                copy_week_schedule.tuesday, section_block
+            )
+
+        elif day == "W":
+            time_conflict_flag = add_time_block_to_day(
+                copy_week_schedule.wednesday, section_block
+            )
+
+        elif day == "J":
+            time_conflict_flag = add_time_block_to_day(
+                copy_week_schedule.thursday, section_block
+            )
+
+        elif day == "V":
+            time_conflict_flag = add_time_block_to_day(
+                copy_week_schedule.friday, section_block
+            )
 
         if time_conflict_flag:
             return (curr_week_schedule, True)
 
     return (copy_week_schedule, False)
 
+
 # Adds one time block to the specified day passed in the parameters. This function takes care of
 # placing the time block in order by its time (->ascending) and checks for time conflicts if any.
-def add_time_block_to_day(week_day : list[TimeBlock], new_section_block : TimeBlock) -> bool:
+def add_time_block_to_day(
+    week_day: list[TimeBlock], new_section_block: TimeBlock
+) -> bool:
     temp_stack = []
     time_conclict_flag = False
-  
+
     while len(week_day) != 0:
         top = week_day[-1]
 
-        if top.end_time < new_section_block.start_time: break
+        if top.end_time < new_section_block.start_time:
+            break
 
         if check_time_conflict(top, new_section_block):
             time_conclict_flag = True
@@ -153,6 +233,10 @@ def add_time_block_to_day(week_day : list[TimeBlock], new_section_block : TimeBl
 
 
 # Return true if the time blocks conflict or overlap
-def check_time_conflict(locked_time_block : TimeBlock, new_time_block : TimeBlock) -> bool:
-    return locked_time_block.end_time > new_time_block.start_time and \
-           locked_time_block.start_time < new_time_block.end_time
+def check_time_conflict(
+    locked_time_block: TimeBlock, new_time_block: TimeBlock
+) -> bool:
+    return (
+        locked_time_block.end_time > new_time_block.start_time
+        and locked_time_block.start_time < new_time_block.end_time
+    )
--- ssh-scraper/utils_test.py
+++ ssh-scraper/utils_test.py
@@ -2,11 +2,14 @@ from enums import Term
 
 from utils import *
 
+
 def course_section_to_str(section):
-        return f"Course Id: {section.course_id}, Course Name: {section.course_name}, Section: {section.section}, Professor: {section.professor}"
+    return f"Course Id: {section.course_id}, Course Name: {section.course_name}, Section: {section.section}, Professor: {section.professor}"
+
 
 def room_schedule_to_str(schedule):
     return f"Room: {schedule.room}, Days: {schedule.days}, Time: {schedule.start_time}-{schedule.end_time}"
+
 
 class TestUtils:
     def setup_method(self):
@@ -25,7 +28,9 @@ class TestUtils:
         assert section.year == self.year
 
     def test_get_room_schedules(self):
-        course_section_id = get_course_sections(self.course_id, self.term, self.year)[0].id
+        course_section_id = get_course_sections(self.course_id, self.term, self.year)[
+            0
+        ].id
         room_schedules = get_room_schedules(course_section_id)
         assert len(room_schedules) != 0
 
@@ -33,7 +38,9 @@ class TestUtils:
         assert room_schedule.course_section_id == course_section_id
 
     def test_get_course_section_time_blocks(self):
-        blocks = get_course_section_time_blocks(self.course_id, self.section, self.term, self.year)
+        blocks = get_course_section_time_blocks(
+            self.course_id, self.section, self.term, self.year
+        )
         assert len(blocks) != 0
 
         block = blocks[0]
@@ -46,12 +53,14 @@ class TestUtils:
     def test_validate_course_id_false(self):
         assert not validate_course_id("INVALID")
 
+
 # ================================
 # Tests for Schedule generation
 # ================================
 
+
 # Courses:
-#  INSO4116: 
+#  INSO4116:
 #    sections = [070 1:30pm-2:20pm LWV S113]
 #
 #  ININ4020
@@ -62,10 +71,10 @@ class TestUtils:
 #
 class TestSchedulingFunctions:
     def setup_method(self):
-        self.courses1 = ['INSO4116']
-        self.courses2 = ['INSO4116', 'ININ4020']
-        self.courses3 = ['INSO4116', 'ININ4020', 'MUSI3171']
-        self.courses4 = ['ININ4020']
+        self.courses1 = ["INSO4116"]
+        self.courses2 = ["INSO4116", "ININ4020"]
+        self.courses3 = ["INSO4116", "ININ4020", "MUSI3171"]
+        self.courses4 = ["ININ4020"]
 
     def test_make_all_schedules_empty_courses(self):
         schedules = make_all_schedules([], Term.FIRST_SEMESTER, 2023)
@@ -79,6 +88,7 @@ class TestSchedulingFunctions:
         assert len(schedules[0].wednesday) == 1
         assert len(schedules[0].thursday) == 0
         assert len(schedules[0].friday) == 1
+
     def test_make_all_schedules_single_course_MJ(self):
         schedules = make_all_schedules(self.courses4, Term.FIRST_SEMESTER, 2023)
         assert len(schedules) == 2
