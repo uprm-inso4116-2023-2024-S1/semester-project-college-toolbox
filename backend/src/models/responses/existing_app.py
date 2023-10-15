1a936814683bdf28409be1da5c3ef7449400f212
JannGarcia <60623626+JannGarcia@users.noreply.github.com>
2023-10-15 10:46:53 -0400

    Add Application API endpoints for Existing Applications (#148)
    * Add API Endpoint for ExistingApplications
    
    * Add tests for ExistingApplication endpoint
    
    * Formatted files
    
    * Close DB after testing

---
+++ backend/src/models/responses/existing_app.py
@@ -0,0 +1,12 @@
+# src/models/responses/existing_app.py
+from pydantic import BaseModel
+
+
+class ExistingApplicationResponse(BaseModel):
+    Name: str | None = None
+    Description: str
+    URL: str
+    Icon: str
+    Type: str
+    Rating: int
+    RatingCount: int
