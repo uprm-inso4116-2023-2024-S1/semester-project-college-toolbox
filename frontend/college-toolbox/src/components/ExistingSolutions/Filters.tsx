09d2a55c78696036c60f96ef5f822f32472dd148
Jeremy Sanchez <jeremy,sanchez1@upr.edu>
2023-10-29 07:00:30 -0400

    re-styled the filters component

--- frontend/college-toolbox/src/components/ExistingSolutions/Filters.tsx
+++ frontend/college-toolbox/src/components/ExistingSolutions/Filters.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { API_URL } from '../../app/constants';
 
-const Filters: React.FC<{ applications: any[] }> = ({ applications }) => {
+const Filters: React.FC = () => {
 	const [filterList, setCheckedFilters] = useState<string[]>([]);
 
 	const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const value = event.target.value;
 		if (event.target.checked) {
+			// For mutually exclusive checkboxes
+			if (value === 'Price' || value === 'A-Z') {
+				setCheckedFilters((prevChecked) => prevChecked.filter((item) => item !== 'Price' && item !== 'A-Z'));
+			}
+
 			// Checkbox was checked, add its value to the array
 			setCheckedFilters((prevChecked) => [...prevChecked, value]);
 		} else {
@@ -14,120 +19,63 @@ const Filters: React.FC<{ applications: any[] }> = ({ applications }) => {
 			setCheckedFilters((prevChecked) => prevChecked.filter((item) => item !== value));
 		}
 	};
-	
-	const mapFiltersToApps = () => {
-		let filteredApplications = applications.map(app => {
-			for(let filter in filterList){
-				if(app.Type == filter){
-					return app;
-				}
-			}
-		});
-		return filteredApplications;
-	};
 
 	const handleSubmitEvent = () => {
-		applications = mapFiltersToApps();
 		//Uncheck the boxes and clear the list
-		for(let filterString in filterList){
-			const checkbox = document.getElementById(filterString) as HTMLInputElement;
-			if(checkbox!==null) {
-				checkbox.checked = false;
-			} else {
-				console.error("Input element with id "+ filterString + " was not found.");
-			}	
-		setCheckedFilters([]);
-		}
-	}
+
+		
+	};
 
 
     return (
-        <div className="bg-gray-200 rounded-lg p-4">
-            <h2 className="text-xl font-semibold mb-2">Filters</h2>
-             {/* Filter type */}
-						 <div id="type" className="border-black border p-2">
-							<h3 className="text-xl font-medium mb-2">Type:</h3>
-							{/* sub elements */}
-							<div className="p-4">
-								<label htmlFor="Note-taking" className="flex items-center">
-									<input type="checkbox" id="Note-taking" value="Note-taking" onChange={handleCheckboxChange} checked={filterList.includes('Note-taking')}  className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Note-taking</span>
-								</label>
+        <div className="bg-gray-200 rounded-lg p-4 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+            <h2 className="text-2xl font-bold mb-2">Filters</h2>
+            {/* Filter type */}
+            <div id="type" className="border-gray-300 border-2 p-2 dark:border-gray-600">
+                <h3 className="text-xl font-medium mb-1">Type:</h3>
+                <div className="p-4">
+                    {['Note-taking', 'Organizational', 'Study', 'Information', 'Proofreading', 'Budgetting'].map((filterType) => (
+                        <label htmlFor={filterType} className="flex items-center mt-1" key={filterType}>
+                            <input type="checkbox" id={filterType} value={filterType} onChange={handleCheckboxChange} checked={filterList.includes(filterType)} className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-500"/>
+                            <span className="ml-2 text-m font-medium text-gray-900 dark:text-gray-300">{filterType}</span>
+                        </label>
+                    ))}
+                </div>
+            </div>
 
-								<label htmlFor="Organizational" className="flex items-center mt-2">
-									<input type="checkbox" id="Organizational" value="Organizational" onChange={handleCheckboxChange} checked={filterList.includes('Organizational')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Organizational</span>
-								</label>
+            {/* Filter sort */}
+            <div id="sort" className="border-gray-300 border-2 p-2 dark:border-gray-600">
+                <h3 className="text-xl font-medium mb-1">Sort by:</h3>
+                <div className="p-4">
+                    <label htmlFor="High to low" className="flex items-center mt-1">
+                        <input type="checkbox" id="High to low" value= "High to low" onChange={handleCheckboxChange} checked={filterList.includes("High to low")} className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-500"/>
+                        <span className="ml-2 text-m font-medium text-gray-900 dark:text-gray-300">High to low</span>
+                    </label>
 
-								<label htmlFor="Study" className="flex items-center mt-2">
-									<input type="checkbox" id="Study" value="Study" onChange={handleCheckboxChange} checked={filterList.includes('Study')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Study</span>
-								</label>
+                    {['A-Z', 'Price'].map((sortOption) => (
+                        <label htmlFor={sortOption} className="flex items-center mt-1" key={sortOption}>
+                            <input type="checkbox" id={sortOption} value={sortOption} onChange={handleCheckboxChange} checked={filterList.includes(sortOption)} className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded-full focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-500"/>
+                            <span className="ml-2 text-m font-medium text-gray-900 dark:text-gray-300">{sortOption}</span>
+                        </label>
+                    ))}
+                </div>
+            </div>
 
-								<label htmlFor="Information" className="flex items-center">
-									<input type="checkbox" id="Information" value="Information" onChange={handleCheckboxChange} checked={filterList.includes('Information')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Information</span>
-								</label> 	
+            {/* Filter cost */}
+            <div id="cost" className="border-gray-300 border-2 p-2 dark:border-gray-600">
+                <h3 className="text-xl font-medium mb-1">Cost:</h3>
+                <div className="p-4">
+                    {['Free', 'One-Time Buy', 'Subscription'].map((costOption) => (
+                        <label htmlFor={costOption} className="flex items-center mt-1" key={costOption}>
+                            <input type="checkbox" id={costOption} value={costOption} onChange={handleCheckboxChange} checked={filterList.includes(costOption)} className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-500"/>
+                            <span className="ml-2 text-m font-medium text-gray-900 dark:text-gray-300">{costOption}</span>
+                        </label>
+                    ))}
+                </div>
+            </div>
 
-								<label htmlFor="Proofreading" className="flex items-center mt-2">
-									<input type="checkbox" id="Proofreading" value="Proofreading" onChange={handleCheckboxChange} checked={filterList.includes('Proofreading')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Proofreading</span>
-								</label>
-
-								<label htmlFor="Budgetting" className="flex items-center">
-									<input type="checkbox" id="Budgetting" value="Budgetting" onChange={handleCheckboxChange} checked={filterList.includes('Budgetting')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Budgetting</span>
-								</label> 
-
-								<label htmlFor="Other" className="flex items-center mt-2">
-									<input type="checkbox" id="Other" value="Other" onChange={handleCheckboxChange} checked={filterList.includes('Other')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Other</span>
-								</label>
-								
-							</div>
-						 </div>
-
-						 {/* Filter type */}
-						 <div id="sort" className="border-black border p-2">
-							<h3 className="text-xl font-medium mb-2">Sort by:</h3>
-							<h3 className="text-l font-medium mb-2">Ascending / Descending</h3>
-							{/* sub elements */}
-							<div className="p-4">
-								<label htmlFor="A-Z" className="flex items-center">
-									<input type="checkbox" id="A-Z" value="A-Z" onChange={handleCheckboxChange} checked={filterList.includes('A-Z')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">A-Z<span>&uarr;</span></span>
-								</label> 	
-
-								<label htmlFor="Price" className="flex items-center mt-2">
-									<input type="checkbox" id="Price" value="Price" onChange={handleCheckboxChange} checked={filterList.includes('Price')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Price<span>&darr;</span></span>
-								</label>
-							</div>
-						 </div>
-
-						 {/* Filter type */}
-						 <div id="cost" className="border-black border p-2">
-							<h3 className="text-xl font-medium mb-2">Cost</h3>
-							{/* sub elements */}
-							<div className="p-4">
-								<label htmlFor="Free" className="flex items-center">
-									<input type="checkbox" id="Free" value="Free" onChange={handleCheckboxChange} checked={filterList.includes('Free')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Free</span>
-								</label> 
-
-								<label htmlFor="One-Time Buy" className="flex items-center">
-									<input type="checkbox" id="One-Time Buy" value="One-Time Buy" onChange={handleCheckboxChange} checked={filterList.includes('One-Time Buy')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">One-Time Buy</span>
-								</label> 
-
-								<label htmlFor="Subscription" className="flex items-center">
-									<input type="checkbox" id="Subscription" value="Subscription" onChange={handleCheckboxChange} checked={filterList.includes('Subscription')} className="form-checkbox text-bg-blue-700 h-5 w-5"/>
-									<span className="ml-2">Subscription</span>
-								</label> 									
-							</div>
-						</div>
-
-						<button type="button" onClick={handleSubmitEvent} className="text-white mt-auto bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Apply</button>
+            <button type="button" onClick={handleSubmitEvent} className="text-white mt-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Apply</button>
+            <button type="button" onClick={() => { setCheckedFilters([]); }} className="text-white mt-1 ml-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Clear</button>
         </div>
     );
 }
