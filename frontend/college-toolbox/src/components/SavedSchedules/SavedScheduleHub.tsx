27ce1cf1cdd4ff8938101e412e7442d2eb8bffd7
Jeremy Sanchez <jeremy,sanchez1@upr.edu>
2023-11-15 11:14:06 -0400

    Created necessary files for future dev work

---
+++ frontend/college-toolbox/src/components/SavedSchedules/SavedScheduleHub.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect  } from 'react';
+import { API_URL } from '../../app/constants';
+
+const SavedScheduleHub: React.FC = () => {
+
+    return (
+        <div className="Saved-Schedule-Hub-container">
+            heyo
+        </div>
+    );
+}
+
+export default SavedScheduleHub;
