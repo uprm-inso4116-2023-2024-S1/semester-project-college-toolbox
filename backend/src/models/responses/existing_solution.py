d9c306a2d0a785983ab3684108062f59db144792
Kelvin Gonzalez <kelvin.gonzalez11@upr.edu>
2023-11-23 20:36:36 -0400

    Fix merge

--- backend/src/models/responses/existing_solution.py
+++ backend/src/models/responses/existing_solution.py
@@ -5,16 +5,16 @@ from src.models.responses.business_model import BusinessModelResponse
 
 
 class ExistingSolutionResponse(BaseModel):
-    Name: Optional[str] = None
-    Description: Optional[str] = None
-    URL: Optional[str] = None
-    Icon: Optional[str] = None
-    Type: Optional[List[str]] = None
-    Rating: Optional[int] = None
-    RatingCount: Optional[int] = None
-    Pros: Optional[List[str]] = None
-    Cons: Optional[List[str]] = None
-    LastUpdated: Optional[str] = None
-    HasMobile: Optional[bool] = None
-    HasWeb: Optional[bool] = None
-    BusinessModels: Optional[List[BusinessModelResponse]] = []
+    Name: str | None = None
+    Description: str
+    URL: str
+    Icon: str
+    Type: List[str]
+    Rating: int
+    RatingCount: int
+    Pros: List[str]
+    Cons: List[str]
+    LastUpdated: str
+    HasMobile: bool
+    HasWeb: bool
+    BusinessModels: List[BusinessModelResponse] = []
